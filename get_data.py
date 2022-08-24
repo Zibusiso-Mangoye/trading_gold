d31d9a34d1b9f9ac3c14e89cc409d44cbc0eb883
Zibusiso <znmangoye@gmail.com>
2022-08-24 16:20:40 +0200

    got the data

--- get_data.py
+++ get_data.py
@@ -1,6 +1,10 @@
-import requests
 import os
+from sqlite3 import Timestamp
+import requests
 import configparser
+
+import pandas as pd
+import numpy as np
 from dotenv import load_dotenv
 
 # loading enviroment variables defined in the .env file
@@ -19,4 +23,14 @@ PARAMS = {'apikey': os.getenv('APIKEY'),
 r = requests.get(url = config['PARAMS']['BASE_URL'], params = PARAMS)
 
 data = r.json()
-print(data)
+meta_data = data['meta']
+tick_data = data['values']
+symbol = meta_data['symbol']
+interval = meta_data['interval']
+
+tick_dataframe = pd.DataFrame(columns=['Timestamp', 'Open', 'High', 'Low', 'Close'])
+
+for i in range(int(PARAMS['outputsize'])):
+    tick_dataframe.loc[len(tick_dataframe.index)] = [tick_data[i]['datetime'], tick_data[i]['open'], tick_data[i]['high'], tick_data[i]['low'], tick_data[i]['close']]
+    
+tick_dataframe.to_csv("tick_data.csv", index=False, na_rep='NULL')
