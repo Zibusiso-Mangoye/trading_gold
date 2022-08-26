00f631fc6a69efe0747e2e193b577b93fcd5b58f
Zibusiso <znmangoye@gmail.com>
2022-08-26 18:35:24 +0200

    Stated work on airflow service

--- get_data.py
+++ get_data.py
@@ -5,10 +5,7 @@ import configparser
 
 import pandas as pd
 import numpy as np
-from dotenv import load_dotenv
 
-# loading enviroment variables defined in the .env file
-load_dotenv()
 
 # Importing application settings in the settings.ini
 config = configparser.ConfigParser()
@@ -20,17 +17,28 @@ PARAMS = {'apikey': os.getenv('APIKEY'),
           'outputsize': config['PARAMS']['OUTPUT_SIZE']
           }
 
-r = requests.get(url = config['PARAMS']['BASE_URL'], params = PARAMS)
+dth = os.environ.get("APIKEY")
 
-data = r.json()
-meta_data = data['meta']
-tick_data = data['values']
-symbol = meta_data['symbol']
-interval = meta_data['interval']
+print(dth)
+# r = requests.get(url = config['PARAMS']['BASE_URL'], params = PARAMS)
 
-tick_dataframe = pd.DataFrame(columns=['Timestamp', 'Open', 'High', 'Low', 'Close'])
+# data = r.json()
+# meta_data = data['meta']
+# tick_data = data['values']
+# symbol = meta_data['symbol']
+# interval = meta_data['interval']
 
-for i in range(int(PARAMS['outputsize'])):
-    tick_dataframe.loc[len(tick_dataframe.index)] = [tick_data[i]['datetime'], tick_data[i]['open'], tick_data[i]['high'], tick_data[i]['low'], tick_data[i]['close']]
+# print({'datetime': tick_data[0]['datetime'],
+#        'open': tick_data[0]['open'],
+#        'high': tick_data[0]['high'],
+#        'low': tick_data[0]['low'],
+#        'close': tick_data[0]['close']
+#        }
+#     )
+
+# tick_dataframe = pd.DataFrame(columns=['Timestamp', 'Open', 'High', 'Low', 'Close'])
+
+# for i in range(int(PARAMS['outputsize'])):
+#     tick_dataframe.loc[len(tick_dataframe.index)] = [tick_data[i]['datetime'], tick_data[i]['open'], tick_data[i]['high'], tick_data[i]['low'], tick_data[i]['close']]
     
-tick_dataframe.to_csv("tick_data.csv", index=False, na_rep='NULL')
+# tick_dataframe.to_csv("tick_data.csv", index=False, na_rep='NULL')
